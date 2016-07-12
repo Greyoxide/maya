2904062c8b698e3db40cf760681d6731578d911d
forrest@keenrender.com <forrest@keenrender.com>
2016-07-12 11:18:56 -0700

    Automated commit at 2016-07-12 18:18:56 UTC by middleman-deploy 1.0.0

---
+++ javascripts/site_script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	$('.page-hero').height($(window).height()-($('#lead-feature').height()+$('#navigation').height()) )
+
+	$('.blade').click(function() {
+		$('.blade').removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$('#hamburger-helper, #hamburger-helper .inner nav li a').click(function(){
+		$(this).toggleClass("active");
+		$('#mobile-menu').slideToggle();
+	});
+});
